698508e64323b233267d719660d49e7f6920edda
JUNGEEYOU <junge2u@naver.com>
2022-07-20 11:51:51 +0900

    add 1288 solution

--- bitmask/src/Solution_1288.java
+++ bitmask/src/Solution_1288.java
@@ -1,7 +1,7 @@
 /*
   새로운 불면증 치료법
 
-  ㅠ  N의 배수 번호인 양을 세기
+  N의 배수 번호인 양을 세기
 
  */
 import java.util.Scanner;
@@ -15,16 +15,24 @@ class Solution_1288
         Scanner sc = new Scanner(System.in);
         int T;
         T=sc.nextInt();
-		/*
-		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-		*/
 
         for(int test_case = 1; test_case <= T; test_case++)
         {
             int n = sc.nextInt();
-            int result = 0;
-
-            System.out.printf("#%d %d", test_case, result);
+//            int result = 0;
+            int visit = (1 << 10) -1;  // 총 10가지 수 체크 (1111...)
+            int cur = 1;
+            int mul = n;
+            while(visit > 0){
+                mul = n * cur;
+                while(mul > 0) {
+                    int div = mul % 10;           // 현재 자리의 수 얻기
+                    visit = visit & ~(1 << div);  // 현재 자리의 값의 비트값을 얻어 반대로하여 곱하면 해당 값만 제거
+                    mul = mul / 10;
+                }
+                cur += 1;
+            }
+            System.out.printf("#%d %d%n", test_case, (cur - 1) * n);
         }
     }
 }
